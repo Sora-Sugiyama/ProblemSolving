bd6d36504e470b00ec1400e2c97b13dd8b2d06f0
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2025-09-17 21:42:30 +0900

    Update fastPatternMatch.cpp

--- str/fastPatternMatch.cpp
+++ str/fastPatternMatch.cpp
@@ -16,36 +16,45 @@ std::vector<int>computeNext(const std::string &pattern){
     return next;
 }
 
-// Find earliest position where the pattern appears
-int patternMatching(std::string text,std::string pattern){
+std::vector<int> patternMatching(std::string text,std::string pattern){
     const int n=(int)text.size()-1,m=(int)pattern.size()-1;
     
     char a=pattern[1];
     text.push_back('^'); text.push_back(a);
     pattern.push_back('@');
     auto next=computeNext(pattern);
-    next[m+1]=-1;
+    
+    std::vector<int>matchedPosition;
     
     int j=1,k=1;
-    getStarted:{
-        while(text[k]!=a)k++;
-        if(k>n)return -1;
-    }
-    charMatched:{
-        j++;k++;
-    }
-    
-    do{
-        if(text[k]==pattern[j])goto charMatched;
-        j=next[j];
-        if(j==1){
-            goto getStarted;
+    while(k<=n){
+        getStarted:{
+            while(text[k]!=a)k++;
+            if(k>n)return matchedPosition;
         }
-        if(j==0){
-            j=1;k++;
-            goto getStarted;
+        goto loop;
+        
+        charMatched:{
+            j++;k++;
+            if(j>m){
+                j=next[j];
+                matchedPosition.push_back(k-m);
+            }
         }
-    }while(j>0);
-    
-    return k-m;
+        
+        loop:
+        do{
+            if(text[k]==pattern[j])goto charMatched;
+            j=next[j];
+            if(j==1){
+                goto getStarted;
+            }
+            if(j==0){
+                j=1;k++;
+                goto getStarted;
+            }
+        }while(j>0);
+        
+    }
+    return matchedPosition;
 }
