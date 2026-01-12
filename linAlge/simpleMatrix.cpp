028f26748a837b53c734c8e85329e7867bcb2382
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2026-01-12 10:25:27 +0900

    Create simpleMatrix.cpp

---
+++ linAlge/simpleMatrix.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll=long long;
+
+template<class T>
+class mat{
+    vector<vector<T> >arr;
+    int n;
+    
+public:
+    mat(int N=0):n(N),arr(vector<vector<T> >(N,vector<T>(N))){
+        for(int i=0;i<n;i++)arr[i][i]=1;
+    }
+    
+    int size(){
+        return n;
+    }
+    
+    const T& operator()(const int i,const int j)const{
+        return arr[i][j];
+    }
+    
+    T& operator()(const int i,const int j){
+        return arr[i][j];
+    }
+    
+    mat& operator*=(const mat &rhs){
+        mat tmp=*this;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                tmp(i,j)=0;
+                for(int k=0;k<n;k++){
+                    tmp(i,j)=(tmp(i,j)+arr[i][k]*rhs(k,j))%1000;
+                }
+            }
+        }
+        this->arr=tmp.arr;
+        return *this;
+    }
+    
+    mat operator*(const mat &rhs){
+        mat tmp=*this;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                tmp(i,j)=0;
+                for(int k=0;k<n;k++){
+                    tmp(i,j)=(tmp(i,j)+arr[i][k]*rhs(k,j))%1000;
+                }
+            }
+        }
+        return tmp;
+    }
+};
+
+int main(){
+    // You can use like
+    mat<ll> A(3);
+    A(0,0)=1;
+    A(1,1)=1;
+    A(2,2)=1;
+    A=A*A;
+    A*=A;
+    
+    return 0;
+}
